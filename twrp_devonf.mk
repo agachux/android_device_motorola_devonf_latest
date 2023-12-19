2f9ddfd9c505c541792ea60b350fa2c5b191e394
agachux <83849024+agachux@users.noreply.github.com>
2023-12-19 11:37:17 +0100

    Update twrp_devonf.mk

--- twrp_devonf.mk
+++ twrp_devonf.mk
@@ -9,21 +9,32 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from devonf device
 $(call inherit-product, device/motorola/devonf/device.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := devonf
 PRODUCT_NAME := omni_devonf
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g73 5G
 PRODUCT_MANUFACTURER := motorola
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="devonf_g_vext-user 12 T1TNS33.14-90-9-2 bbccfa release-keys"
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
 
 BUILD_FINGERPRINT := motorola/devonf_g_vext/devonf:12/T1TNS33.14-90-9-2/bbccfa:user/release-keys
